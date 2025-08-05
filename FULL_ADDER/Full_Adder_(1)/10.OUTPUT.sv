9bf6c5be0355fb632012066e938c066f8a573a38
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-08-05 23:28:08 +0530

    Create 10.OUTPUT.sv

---
+++ FULL_ADDER/Full_Adder_(1)/10.OUTPUT.sv
@@ -0,0 +1,9 @@
+# run -all
+#  generator class signals
+#  a=1, b=1, c=0, sum =0, carry=0 
+#  driver class signals 
+#  a=1, b=1, c=0, sum =0, carry=0 
+#  monitor class signals 
+#  a=1, b=1, c=0, sum =0, carry=1 
+#  Scoreboard signals 
+#  PASS 
