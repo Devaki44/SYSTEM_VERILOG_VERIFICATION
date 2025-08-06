568da3924f8cd3c00a98f7361b26f2b79630775a
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-08-06 22:58:55 +0530

    Create OUTPUT.sv

---
+++ HA_Adder/HA_ADDER/OUTPUT.sv
@@ -0,0 +1,22 @@
+xcelium> run
+ generator class data
+time=0, a=1,b=0,sum=0,carry=0
+ driver class data
+time=0, a=1,b=0,sum=0,carry=0
+monitor class data
+time=2, a=1,b=0,sum=1,carry=0
+ scoreboard class signals
+time=2, a=1,b=0,sum=1,carry=0
+PASS
+ generator class data
+time=5, a=0,b=1,sum=0,carry=0
+ driver class data
+time=5, a=0,b=1,sum=0,carry=0
+monitor class data
+time=7, a=0,b=1,sum=1,carry=0
+ scoreboard class signals
+time=7, a=0,b=1,sum=1,carry=0
+PASS
+Simulation complete via implicit call to $finish(1) at time 12 NS + 1
+./test.sv:3 program test(intf vintf);
+xcelium> exit
