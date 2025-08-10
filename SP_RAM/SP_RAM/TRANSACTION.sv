69a0e17418db458c7cba684c60c51ff8d0b920ac
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-08-10 10:29:23 +0530

    Update and rename TRANSACTION.sv to TRANSACTION.sv

--- SP_RAM/SP_RAM/TRANSACTION.sv
+++ SP_RAM/SP_RAM/TRANSACTION.sv
@@ -1,10 +1,11 @@
 class transaction;
+  bit clk;
   bit rst;
   bit en;
   randc bit[7:0]data_in;
   randc  bit[3:0] addr;
   bit   [7:0]data_out;
   
-  constraint ad_c{	addr  inside {[2:4]} ;}
+  constraint ad_c{	addr == 4'hf ;}
   constraint in_c {	data_in < 20 ;}  
 endclass
