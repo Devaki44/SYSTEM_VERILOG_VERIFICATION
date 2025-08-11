370f9aa66f3ba17386552c737e210193caa1a1bc
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-08-11 14:56:25 +0530

    Create DESIGN.sv

---
+++ T_FF/T_FF(1)/DESIGN.sv
@@ -0,0 +1,17 @@
+module tff(
+  input clk,
+  input rst,
+  input t,
+  output reg q);
+  
+  always@(posedge clk)begin
+    if(rst)
+      q <= 1'b0 ;
+    else begin
+      if(t)
+       q <= ~q ;
+      else
+       q <= q;
+    end
+  end
+endmodule
