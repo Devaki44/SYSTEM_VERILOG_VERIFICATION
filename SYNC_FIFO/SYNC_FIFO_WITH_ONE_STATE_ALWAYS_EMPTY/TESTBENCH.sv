bf6ebb06b0f77de2ec2766b1360935c42522614a
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-08-12 22:42:34 +0530

    Create TESTBENCH.sv

---
+++ SYNC_FIFO/SYNC_FIFO_WITH_ONE_STATE_ALWAYS_EMPTY/TESTBENCH.sv
@@ -0,0 +1,39 @@
+`include "interface.sv"
+`include "test.sv"
+
+module testbench;
+  logic clk;
+  logic rst;
+  logic en;
+  
+  intf vintf(clk,rst,en);
+  test tst(vintf);
+ fifo dut(
+    .clk(vintf.clk),
+    .rst(vintf.rst),
+    .en(vintf.en),
+    .data_in(vintf.data_in),
+    .data_out(vintf.data_out),
+    .full(vintf.full),
+    .empty(vintf.empty)
+);
+
+  
+  initial begin
+    clk = 0 ;
+    forever #5 clk = ~clk ;
+  end
+  initial begin
+    rst = 1;en = 1;
+    #10;
+    rst = 0;
+    #20;
+    en = 0;
+    #300;
+    $finish;
+  end
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
+endmodule
